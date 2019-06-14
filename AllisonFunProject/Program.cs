1099d1a300c1e6f8331211836ea83805b6d7c348
Allisonlredding <Allisonlredding@gmail.com>
2019-06-14 15:08:14 -0400

    Check in
    Validation

--- AllisonFunProject/Program.cs
+++ AllisonFunProject/Program.cs
@@ -12,25 +12,25 @@ namespace AllisonFunProject
             Console.WriteLine("(c) numbers 20-30");
             Console.WriteLine("(d) numbers 30-40");
             // test was just created as a test and can be removed.  Console.Readline() is that "action" of reading the input
-            var test = Console.ReadLine();
-            
-            if (test == "a")
+            var lETTERSELECTED = Console.ReadLine();
+
+            if (lETTERSELECTED == "a")
             {
                 Console.WriteLine("You've selected numbers 1-10");
                 //test
             }
 
-            else if (test == "b")
+            else if (lETTERSELECTED == "b")
             {
                 Console.WriteLine("You've selected numbers 10-20");
             }
 
-            else if (test == "c")
+            else if (lETTERSELECTED == "c")
             {
                 Console.WriteLine("You've selected numbers 20-30");
             }
 
-            else if (test == "d")
+            else if (lETTERSELECTED == "d")
             {
                 Console.WriteLine("You've selected numbers 30-40");
             }
@@ -41,11 +41,17 @@ namespace AllisonFunProject
             //then set it equal to the readlinefunction
             //BUT, that only brings in the EXACT value INCLUDING CAPITALS
             // create another variable and name it something similar to the first with the addition of letting you know it will be the value in lower case
-            // then set it equal to the other variable with the lowercase method
-            String variable = test.ToLower();
+            // then set it equal to the other variable with the lowercase method                 
+
+
+            String variable = lETTERSELECTED.ToLower();
             Console.WriteLine($"Lowercase: {variable}");
+
+            if (lETTERSELECTED != "a" && lETTERSELECTED != "b" && lETTERSELECTED != "c" && lETTERSELECTED != "d")
+            {
+                Console.WriteLine("Please select a valid option");
+            }
+
         }
     }
-   
-
-    }
+}
